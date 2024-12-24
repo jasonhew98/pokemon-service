ae8b388399cb73a4f798b8e8f3e433dbe46c553d
Jason Hew <jasonhew1211@gmail.com>
2024-12-24 15:38:14 +0800

    Add health controller

---
+++ Api/Controllers/Health/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Controllers.Health;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : Controller
+{
+    public HealthController()
+    {
+    }
+
+    [HttpGet]
+    [Route("")]
+    [ProducesResponseType(typeof(string), (int)HttpStatusCode.Accepted)]
+    public async Task<IActionResult> Health()
+    {
+        return Ok("Ok");
+    }
+}
